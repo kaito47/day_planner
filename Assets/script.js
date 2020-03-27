75951249acd4a8875ffb9c4d33db32b277dfcdab
Kait OShaughnessy <oshaughnessykm@gmail.com>
2020-03-27 19:49:48 -0400

    added moment.js elements

--- Assets/script.js
+++ Assets/script.js
@@ -9,9 +9,22 @@ console.log(moment());
 //Obtaining today's date from moment.js
 let  date = moment().format("dddd");
 console.log(date);
-//selecting the html ID
-let dayTag = document.querySelector("day");
-//inserting date into html h1 element
-dayTag.innerHTML = "Hola"; 
 
-document.getElementById("day").innerHTML = "Hi there!"; 
+//selecting the html ID
+let dayTag = document.querySelector("#day");
+
+//inserting date into html h2 element
+dayTag.append(date);
+
+
+//inserting time and date into p element
+let timeDate = document.querySelector("#timeDate")
+timeDate.innerHTML = moment().format("LLL");
+
+
+// moment().format("MMMM Do YYYY, h:mm:ss a"));
+
+
+
+
+//troubleshooting

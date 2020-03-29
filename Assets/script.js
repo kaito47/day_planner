f6f636e53d0b6a9f94c4d137906bd9dc5435d976
Kait OShaughnessy <oshaughnessykm@gmail.com>
2020-03-29 15:35:19 -0400

    local storage added to all rows

--- Assets/script.js
+++ Assets/script.js
@@ -22,101 +22,136 @@ let timeDate = document.querySelector("#timeDate")
 timeDate.innerHTML = moment().format("LLL");
 
 //get stored objects
-setText();
+// setText();
 
-// let input6a = $('.input6a');
-//function for getting stored object
-// function renderLastRegistered() {
-//   var userInput6a = localStorage.getItem("text6a");
-//   $('.input6a').innerHTML = userInput6a;
-// }
-function setText() {
-  var savedText = localStorage.getItem('text6a');
-  $('#input6a').append(savedText);
+// function setText() {
   
-}
+  var savedText7a = localStorage.getItem('text7a');
+  $('#input7a').append(savedText7a);
+
+  var savedText8a = localStorage.getItem('text8a');
+  $('#input8a').append(savedText8a);
+
+  var savedText9a = localStorage.getItem('text9a');
+  $('#input9a').append(savedText9a);
+
+  var savedText10a = localStorage.getItem('text10a');
+  $('#input10a').append(savedText10a);
+
+  var savedText11a = localStorage.getItem('text11a');
+  $('#input11a').append(savedText11a);
+
+  var savedText12p = localStorage.getItem('text12p');
+  $('#input12p').append(savedText12p);
+
+  var savedText1p = localStorage.getItem('text1p');
+  $('#input1p').append(savedText1p);
+
+  var savedText2p = localStorage.getItem('text2p');
+  $('#input2p').append(savedText2p);
+
+  var savedText3p = localStorage.getItem('text3p');
+  $('#input3p').append(savedText3p);
+
+  var savedText4p = localStorage.getItem('text4p');
+  $('#input4p').append(savedText4p);
+
+  var savedText5p = localStorage.getItem('text5p');
+  $('#input5p').append(savedText5p);
+
+
+// }
 //selecting html button
 // let saveBtn = document.querySelectorAll('.saveBtn');
 
 
 //selecting user input with on click
-$('.saveBtn').on('click', function(event) {
+$('#saveBtn7a').on('click', function(event) {
   event.preventDefault();
-  var text6a = $('#input6a').val();
-  console.log(text6a);
+  var text7a = $('#input7a').val();
+  console.log(text7a);
 
-  localStorage.setItem('text6a', text6a);
-  setText();
+  localStorage.setItem('text7a', text7a);
+  // setText();
 })
 
-// $('button').on('click', function(event) {
-//   event.preventDefault();
-//   var userInput7a = $('.input7a').val();
-//   console.log(userInput7a);
-// })
+$('#saveBtn8a').on('click', function(event) {
+  event.preventDefault();
+  var text8a = $('#input8a').val();
+  console.log(text8a);
 
-// $('button').on('click', function(event) {
-//   event.preventDefault();
-//   var userInput8a = $('.input8a').val();
-//   console.log(userInput8a);
-// })
+  localStorage.setItem('text8a', text8a);
+})
 
-// $('button').on('click', function(event) {
-//   event.preventDefault();
-//   var userInput9a = $('.input9a').val();
-//   console.log(userInput9a);
-// })
+$('button').on('click', function(event) {
+  event.preventDefault();
+  var text9a = $('#input9a').val();
+  console.log(text9a);
 
-// $('button').on('click', function(event) {
-//   event.preventDefault();
-//   var userInput10a = $('.input10a').val();
-//   console.log(userInput10a);
-// })
+  localStorage.setItem('text9a', text9a);
+})
 
-// $('button').on('click', function(event) {
-//   event.preventDefault();
-//   var userInput11a = $('.input11a').val();
-//   console.log(userInput11a);
-// })
+$('button').on('click', function(event) {
+  event.preventDefault();
+  var text10a = $('#input10a').val();
+  console.log(text10a);
 
-// $('button').on('click', function(event) {
-//   event.preventDefault();
-//   var userInput12p = $('.input12p').val();
-//   console.log(userInput12p);
-// })
+  localStorage.setItem('text10a', text10a);
+})
 
-// $('button').on('click', function(event) {
-//   event.preventDefault();
-//   var userInput1p = $('.input1p').val();
-//   console.log(userInput1p);
-// })
+$('button').on('click', function(event) {
+  event.preventDefault();
+  var text11a = $('#input11a').val();
+  console.log(text11a);
 
-// $('button').on('click', function(event) {
-//   event.preventDefault();
-//   var userInput2p = $('.input2p').val();
-//   console.log(userInput2p);
-// })
+  localStorage.setItem('text11a', text11a);
+})
 
-// $('button').on('click', function(event) {
-//   event.preventDefault();
-//   var userInput3p = $('.input3p').val();
-//   console.log(userInput3p);
-// })
+$('button').on('click', function(event) {
+  event.preventDefault();
+  var text12p = $('#input12p').val();
+  console.log(text12p);
 
-// $('button').on('click', function(event) {
-//   event.preventDefault();
-//   var userInput4p = $('.input4p').val();
-//   console.log(userInput4p);
-// })
+  localStorage.setItem('text12p', text12p);
+})
 
-// $('button').on('click', function(event) {
-//   event.preventDefault();
-//   var userInput5p = $('.input5p').val();
-//   console.log(userInput5p);
-// })
+$('button').on('click', function(event) {
+  event.preventDefault();
+  var text1p = $('#input1p').val();
+  console.log(text1p);
 
-// $('button').on('click', function(event) {
-//   event.preventDefault();
-//   var userInput6p = $('.input6p').val();
-//   console.log(userInput6p);
-// });
+  localStorage.setItem('text1p', text1p);
+})
+
+$('button').on('click', function(event) {
+  event.preventDefault();
+  var text2p = $('#input2p').val();
+  console.log(text2p);
+
+  localStorage.setItem('text2p', text2p);
+})
+
+$('button').on('click', function(event) {
+  event.preventDefault();
+  var text3p = $('#input3p').val();
+  console.log(text3p);
+
+  localStorage.setItem('text3p', text3p);
+})
+
+$('button').on('click', function(event) {
+  event.preventDefault();
+  var text4p = $('#input4p').val();
+  console.log(text4p);
+
+  localStorage.setItem('text4p', text4p);
+})
+
+$('button').on('click', function(event) {
+  event.preventDefault();
+  var text5p = $('#input5p').val();
+  console.log(text5p);
+
+  localStorage.setItem('text5p', text5p);
+})
+
